976b12e29f9e522ec1ebbb87a5358a4d5fd5dc20
Sagar Jadhav <sagarmj1502@gmail.com>
2025-08-02 18:05:52 +0530

    Add welcome route to the application

--- src/app.js
+++ src/app.js
@@ -6,5 +6,8 @@ const authRoute = require('./routes/auth.route');
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.get('/', (req, res) => {
+    res.send('Welcome in the Social Media App');
+});
 app.use('/auth', authRoute);
 module.exports = app;
